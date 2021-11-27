af9ea5bca34c0d5431ddbc51362a74a20e851eb0
SERCAN <sercan_saglam19@hotmail.com>
2021-11-28 02:25:36 +0300

    commit

--- Space Shooter Game/Assets/Scripts/PlayerController.cs
+++ Space Shooter Game/Assets/Scripts/PlayerController.cs
@@ -2,35 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
 public class PlayerController : MonoBehaviour
 {
-    //private float hiz = 5f;
+    
     Rigidbody physic;
-    public int speed;
-    public float xMin, xMax, zMin, zMax;
+    [SerializeField] int speed;   // Bu deðiþkene Unity'den eriþmek istiyorum. Lakin baþka bir yerden de eriþilmesini istemiyorsam;
+    [SerializeField] int tilt;
+    public Boundary boundary;
+    
     void Start()
     {
         physic = GetComponent<Rigidbody>();
     }
 
     
-    void FixedUpdate()
+    void FixedUpdate()  //Fizik iþlermleriden fixedupdate kullanýlmaktadýr.
     {
         float movehorizontal = Input.GetAxis("Horizontal");
         float movevertical = Input.GetAxis("Vertical");
-
         Vector3 movement = new Vector3(movehorizontal, 0f, movevertical);
-
         physic.velocity = movement * speed;
         //transform.Translate(horizontal * Time.deltaTime * hiz, 0f, vertical * Time.deltaTime * hiz); 
         //Performans kaybý olacaðý için bunun yerine Rigidbody-Velocity kullanýyoruz.
 
         Vector3 position = new Vector3(
-            Mathf.Clamp(transform.position.x, xMin, xMax),  // kendi pozisyonu, min gidebileceði x noktasý, max gidebileceði x noktasý
+            Mathf.Clamp(transform.position.x, boundary.xMin, boundary.xMax),  //Kendi pozisyonu, min gidebileceði x noktasý, max gidebileceði x noktasý
             0.832f,
-            Mathf.Clamp(transform.position.z, zMin, zMax));
+            Mathf.Clamp(transform.position.z, boundary.zMin, boundary.zMax));
         transform.position = position;
         // Sýnýrlarý Collider yapmadan kod ile bu þekilde yapabiliyoruz.
+
+        transform.rotation = Quaternion.Euler(0, 0, physic.velocity.x * tilt);  // Saða-sola eðim vererek hareket ettirme kodu.
 
 
     }
