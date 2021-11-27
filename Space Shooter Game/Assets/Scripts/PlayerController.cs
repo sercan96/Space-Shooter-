e8dd9547caae3c579233cd3fff00db5e3b606459
SERCAN <sercan_saglam19@hotmail.com>
2021-11-27 18:27:09 +0300

    commit

--- Space Shooter Game/Assets/Scripts/PlayerController.cs
+++ Space Shooter Game/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     //private float hiz = 5f;
     Rigidbody physic;
     public int speed;
+    public float xMin, xMax, zMin, zMax;
     void Start()
     {
         physic = GetComponent<Rigidbody>();
@@ -20,10 +21,17 @@ public class PlayerController : MonoBehaviour
 
         Vector3 movement = new Vector3(movehorizontal, 0f, movevertical);
 
-
         physic.velocity = movement * speed;
-
         //transform.Translate(horizontal * Time.deltaTime * hiz, 0f, vertical * Time.deltaTime * hiz); 
         //Performans kaybý olacaðý için bunun yerine Rigidbody-Velocity kullanýyoruz.
+
+        Vector3 position = new Vector3(
+            Mathf.Clamp(transform.position.x, xMin, xMax),  // kendi pozisyonu, min gidebileceði x noktasý, max gidebileceði x noktasý
+            0.832f,
+            Mathf.Clamp(transform.position.z, zMin, zMax));
+        transform.position = position;
+        // Sýnýrlarý Collider yapmadan kod ile bu þekilde yapabiliyoruz.
+
+
     }
 }
